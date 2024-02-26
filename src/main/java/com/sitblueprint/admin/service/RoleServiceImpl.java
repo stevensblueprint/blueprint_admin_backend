000fc25368e9fb92caa11b75cbe2129b22dab9b1
miguel-merlin <mmerlin@stevens.edu>
2024-02-26 03:40:33 -0500

    feat: Added Role and Team service impl

---
+++ src/main/java/com/sitblueprint/admin/service/RoleServiceImpl.java
@@ -0,0 +1,32 @@
+package com.sitblueprint.admin.service;
+
+import com.sitblueprint.admin.model.Role;
+import com.sitblueprint.admin.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    RoleRepository roleRepository;
+    @Override
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Role getRoleById(Long roleId) {
+        return roleRepository.findById(roleId).get();
+    }
+
+    @Override
+    public Role createRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void deleteRole(Long roleId) {
+        roleRepository.deleteById(roleId);;
+    }
+}
---
+++ src/main/java/com/sitblueprint/admin/service/TeamServiceImpl.java
@@ -0,0 +1,57 @@
+package com.sitblueprint.admin.service;
+
+import com.sitblueprint.admin.model.Team;
+import com.sitblueprint.admin.model.User;
+import com.sitblueprint.admin.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TeamServiceImpl implements TeamService {
+
+    @Autowired
+    TeamRepository teamRepository;
+    @Override
+    public List<Team> getAllTeams() {
+        return teamRepository.findAll();
+    }
+
+    @Override
+    public Team getTeamById(Long teamId) {
+        return teamRepository.findById(teamId).get();
+    }
+
+    @Override
+    public Team createTeam(Team team) {
+        return teamRepository.save(team);
+    }
+
+    @Override
+    public Team updateTeam(Team team) {
+        return teamRepository.saveAndFlush(team);
+    }
+
+    @Override
+    public void deleteTeam(Long teamId) {
+        teamRepository.deleteById(teamId);
+    }
+
+    @Override
+    public User getTeamLeadById(Long teamId) {
+        Optional<Team> optionalTeam = teamRepository.findById(teamId);
+        if (!optionalTeam.isPresent()) {
+            throw new RuntimeException("Team not found: " + teamId);
+        }
+        return optionalTeam.get().getTeamLead();
+    }
+
+    @Override
+    public User getProductManagerById(Long teamId) {
+        Optional<Team> optionalTeam = teamRepository.findById(teamId);
+        if (!optionalTeam.isPresent()) {
+            throw new RuntimeException("Team not found: " + teamId);
+        }
+        return optionalTeam.get().getProductManager();
+    }
+}
