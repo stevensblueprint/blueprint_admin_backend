93f2c683ed314f71df9003dc2331cd153d359015
miguel-merlin <mmerlin@stevens.edu>
2024-02-26 03:54:30 -0500

    feat: Added role controllers

--- src/main/java/com/sitblueprint/admin/controller/RoleController.java
+++ src/main/java/com/sitblueprint/admin/controller/RoleController.java
@@ -1,4 +1,37 @@
 package com.sitblueprint.admin.controller;
 
+import com.sitblueprint.admin.model.Role;
+import com.sitblueprint.admin.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/role/")
 public class RoleController {
+
+    @Autowired
+    RoleService roleService;
+
+    @GetMapping("all")
+    public List<Role> getAllRoles() {
+        return roleService.getAllRoles();
+    }
+
+    @GetMapping
+    public Role getRoleById(@Param("roleId") String roleId) {
+        return roleService.getRoleById(Long.parseLong(roleId));
+    }
+
+    @PostMapping
+    public Role createRole(@RequestBody Role role) {
+        return roleService.createRole(role);
+    }
+
+    @DeleteMapping
+    public void deleteRole(String roleId) {
+        roleService.deleteRole(Long.parseLong(roleId));
+    }
 }
--- src/main/java/com/sitblueprint/admin/service/RoleServiceImpl.java
+++ src/main/java/com/sitblueprint/admin/service/RoleServiceImpl.java
@@ -3,9 +3,10 @@ package com.sitblueprint.admin.service;
 import com.sitblueprint.admin.model.Role;
 import com.sitblueprint.admin.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class RoleServiceImpl implements RoleService {
 
     @Autowired
