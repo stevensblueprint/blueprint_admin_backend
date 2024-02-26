9e9fff62581e70ffaf39594678e02654ad10c0d3
miguel-merlin <mmerlin@stevens.edu>
2024-02-26 03:50:53 -0500

    feat: Added get team lead controller in Team Controller

--- src/main/java/com/sitblueprint/admin/controller/TeamController.java
+++ src/main/java/com/sitblueprint/admin/controller/TeamController.java
@@ -1,11 +1,11 @@
 package com.sitblueprint.admin.controller;
 
 import com.sitblueprint.admin.model.Team;
+import com.sitblueprint.admin.model.User;
 import com.sitblueprint.admin.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,35 @@ public class TeamController {
     @GetMapping("all")
     public List<Team> getAllTeams() {
         return teamService.getAllTeams();
+    }
+
+    @GetMapping
+    public Team getTeam(@Param("teamId") String teamId) {
+        return teamService.getTeamById(Long.parseLong(teamId));
+    }
+
+    @PostMapping
+    public Team createTeam(@RequestBody Team team) {
+        return teamService.createTeam(team);
+    }
+
+    @PutMapping
+    public Team updateTeam(@RequestBody Team team) {
+        return teamService.updateTeam(team);
+    }
+
+    @DeleteMapping
+    public void deleteTeam(String teamId) {
+        teamService.deleteTeam(Long.parseLong(teamId));
+    }
+
+    @GetMapping
+    public User getTeamLeadById(@Param("teamId") String teamId) {
+        return teamService.getTeamLeadById(Long.parseLong(teamId));
+    }
+
+    @GetMapping
+    public User getProductManagerById(@Param("productId") String productId) {
+        return teamService.getProductManagerById(Long.parseLong(productId));
     }
 }
