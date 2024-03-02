49f627409839db0b95dd34e389fd1d37156b23bb
miguel-merlin <mmerlin@stevens.edu>
2024-03-02 18:59:10 -0500

    styling: Removed unused import

--- src/main/java/com/sitblueprint/admin/service/AuthApiServiceImpl.java
+++ src/main/java/com/sitblueprint/admin/service/AuthApiServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Type;
 import java.util.List;
 
 @Service
