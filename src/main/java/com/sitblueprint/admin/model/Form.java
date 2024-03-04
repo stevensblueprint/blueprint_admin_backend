3f4dcbf4ab36cf23f4b50f3ea6bb0e28e48e2909
miguel-merlin <mmerlin@stevens.edu>
2024-03-03 20:30:05 -0500

    feat: Added Form super class

---
+++ src/main/java/com/sitblueprint/admin/model/Form.java
@@ -0,0 +1,9 @@
+package com.sitblueprint.admin.model;
+
+import java.time.LocalDateTime;
+
+public class Form {
+    private String name;
+    private String email;
+    private LocalDateTime dateCreated;
+}
