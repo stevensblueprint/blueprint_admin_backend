02918ac4fde5c0b7c13420d1d2525ac29b586863
miguel-merlin <mmerlin@stevens.edu>
2024-03-03 20:30:48 -0500

    feat: Added annotation to Form class

--- src/main/java/com/sitblueprint/admin/model/Form.java
+++ src/main/java/com/sitblueprint/admin/model/Form.java
@@ -1,7 +1,14 @@
 package com.sitblueprint.admin.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Form {
     private String name;
     private String email;
