2bf42e770b9b8f35109791272531df812241ad10
Eric Zhang <ericzhang200@gmail.com>
2024-04-02 19:38:57 -0400

    Change interface name from AttendanceRepository to AttendanceRecordRepository to reflect actual database name

--- src/main/java/com/sitblueprint/admin/repository/users/AttendenceRecordRepository.java
+++ src/main/java/com/sitblueprint/admin/repository/users/AttendenceRecordRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+public interface AttendanceRecordRepository extends JpaRepository<Attendance, Long> {
 }
