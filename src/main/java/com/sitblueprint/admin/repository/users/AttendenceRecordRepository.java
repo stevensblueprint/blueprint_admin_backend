a3de4ec4919457d14b9f893614196b8d6c3b3e40
Eric Zhang <ericzhang200@gmail.com>
2024-04-02 08:15:06 -0400

    Accidently put User repository as Attendance repository. made it its own repository

--- src/main/java/com/sitblueprint/admin/repository/users/AttendenceRecordRepository.java
+++ src/main/java/com/sitblueprint/admin/repository/users/AttendenceRecordRepository.java
@@ -1,9 +1,9 @@
 package com.sitblueprint.admin.repository.users;
 
-import com.sitblueprint.admin.model.users.User;
+import com.sitblueprint.admin.model.users.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 }
