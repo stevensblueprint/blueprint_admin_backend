c09f58ad92e845615e75f076ae1adb57bf73f751
Eric Zhang <ericzhang200@gmail.com>
2024-04-03 03:21:24 -0400

    Fixed spelling

--- src/main/java/com/sitblueprint/admin/controller/AttendanceController.java
+++ src/main/java/com/sitblueprint/admin/controller/AttendanceController.java
@@ -16,7 +16,7 @@ public class AttendanceController {
     UserService userService;
 
     @GetMapping("/{attendanceId}")
-    public List<Attendance> getAttendanceById(@PathVariable("attendanceId") String attendanceId) {
+    public Attendance getAttendanceById(@PathVariable("attendanceId") String attendanceId) {
         return userService.getAttendanceById(Long.parseLong(attendanceId));
     }
 	
