3b9e45bff4ef771ba977387e301ca883cee5ef15
miguel-merlin <mmerlin@stevens.edu>
2024-04-07 22:47:05 -0400

    refactor: exception annotaion

--- src/main/java/com/sitblueprint/admin/controller/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/sitblueprint/admin/controller/exceptions/GlobalExceptionHandler.java
@@ -3,24 +3,26 @@ package com.sitblueprint.admin.controller.exceptions;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.NoSuchElementException;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler
-    public ResponseEntity handleServiceException(ServiceException e, WebRequest request) {
-        return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<String> handleServiceException(ServiceException e, WebRequest request) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler
-    public ResponseEntity handleNoSuchElementException(NoSuchElementException e, WebRequest request) {
-        return new ResponseEntity("Resource not found", HttpStatus.NOT_FOUND);
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e, WebRequest request) {
+        return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity handleGenericException(Exception e, WebRequest request) {
-        return new ResponseEntity("An unexpected error has occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> handleGenericException(Exception e, WebRequest request) {
+        return new ResponseEntity<String>("An unexpected error has occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
