a11474c327f093e4aa30036aea30fa754bf1e1b1
miguel-merlin <mmerlin@stevens.edu>
2024-04-11 11:41:34 -0400

    styling

--- src/main/java/com/sitblueprint/admin/config/WebConfig.java
+++ src/main/java/com/sitblueprint/admin/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://admin.sitblueprint.com")  // Adjust as needed
+                .allowedOrigins("http://admin.sitblueprint.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
