02de6c0ec50dac89a7b76a78535b164ca3be61f8
miguel-merlin <mmerlin@stevens.edu>
2024-04-12 14:00:04 -0400

    feat: Changed to https

--- src/main/java/com/sitblueprint/admin/config/WebConfig.java
+++ src/main/java/com/sitblueprint/admin/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://admin.sitblueprint.com")
+                .allowedOrigins("https://admin.sitblueprint.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
