e87f6595b621270ef76f0ba8dd0714b7f2c8167a
Bradley Griffel <griffelbw@gmail.com>
2024-10-09 19:04:20 -0400

    started adding the assigned team in NPO

--- src/main/java/com/sitblueprint/admin/model/npos/NPO.java
+++ src/main/java/com/sitblueprint/admin/model/npos/NPO.java
@@ -1,5 +1,19 @@
 package com.sitblueprint.admin.model.npos;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.sitblueprint.admin.model.users.Team;
+
 public class NPO {
     private String name;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn()
+    private Team team_assigned;
+
+
+
+
 }
