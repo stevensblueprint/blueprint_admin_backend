0a4fcd3bffb3aff3c9bfdb5d27378598d0c25e3f
Sofia Martinez <sofiacaro1284@gmail.com>
2024-10-22 13:06:57 -0400

    added CRUD logic in blog service

--- src/main/java/com/sitblueprint/admin/service/blogs/BlogsService.java
+++ src/main/java/com/sitblueprint/admin/service/blogs/BlogsService.java
@@ -5,4 +5,12 @@ import com.sitblueprint.admin.model.blogs.Blog;
 import java.util.List;
 public interface BlogsService {
     List<Blog> getAllBlogs();
+
+    Blog getBlogById(Long blogId);
+
+    Blog createBlog(Blog blog);
+
+    Blog updateBlog(Blog blog);
+
+    void deleteBlogById(Long blogId);
 }
--- src/main/java/com/sitblueprint/admin/service/blogs/BlogsServiceImpl.java
+++ src/main/java/com/sitblueprint/admin/service/blogs/BlogsServiceImpl.java
@@ -2,7 +2,6 @@ package com.sitblueprint.admin.service.blogs;
 
 import com.sitblueprint.admin.model.blogs.Blog;
 import com.sitblueprint.admin.repository.blogs.BlogRepository;
-import com.sitblueprint.admin.repository.blogs.BlogRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
@@ -25,5 +24,32 @@ public class BlogsServiceImpl implements BlogsService {
     @Override
     public List<Blog> getAllBlogs() {
         return blogRepository.findAll();
+    }
+
+    @Override
+    public Blog getBlogById(Long blogId){
+        return blogRepository.findById(blogId).orElseThrow(
+                () -> new NoSuchElementException("Blog with id " + blogId + " was not found")
+        );
+    }
+
+    @Override
+    public Blog createBlog(Blog blog){
+        blog.setDateCreated(LocalDateTime.now());
+        return blogRepository.save(blog);
+    }
+
+    @Override
+    public Blog updateBlog(Blog blog){
+        return blogRepository.saveAndFlush(blog);
+    }
+
+    @Override
+    public void deleteBlogById(Long blogId){
+        Optional<Blog> optionalBlogToDelete = blogRepository.findById(blogId);
+        if(optionalBlogToDelete.isEmpty()){
+            throw new RuntimeException("Blog with id " + blogId + " was not found");
+        }
+        blogRepository.deleteById(blogId);
     }
 }
