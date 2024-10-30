a750109a98438f827438d3378f356b9db0f526a8
Butter - ThinkWin <butterman423@gmail.com>
2024-10-30 18:32:05 -0400

    Added public access modifier

--- src/main/java/com/sitblueprint/admin/service/users/NPOServiceImpl.java
+++ src/main/java/com/sitblueprint/admin/service/users/NPOServiceImpl.java
@@ -2,24 +2,26 @@ package com.sitblueprint.admin.service.users;
 
 import com.sitblueprint.admin.model.npos.NPO;
 
+import java.util.List;
+
 public class NPOServiceImpl implements NPOService {
-    List<NPO> getAllNPOs() {
+    public List<NPO> getAllNPOs() {
 
     }
 
-    NPO getNPOById(Long id) {
+    public NPO getNPOById(Long id) {
 
     }
 
-    NPO createNPO(NPO npo) {
+    public NPO createNPO(NPO npo) {
 
     }
 
-    NPO updateNPO(NPO npo) {
+    public NPO updateNPO(NPO npo) {
 
     }
 
-    void deleteNPOById(Long id) {
+    public void deleteNPOById(Long id) {
 
     }
 }
