48a3602cc0bef4d01b3ed6e51390094383254744
IvanFarfan08 <ivanfarfan567@yahoo.com>
2024-11-03 00:54:24 -0400

    feat: fixedSQL script

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,65 +1,108 @@
-    -- Enum type for roles
-    CREATE TYPE member_role AS ENUM ('E-BOARD', 'TEAM_LEAD', 'PRODUCT_MANAGER', 'DEVELOPER', 'BLUEPRINT_INTERNAL_TEAM');
+   -- Create roles table
+   CREATE TABLE roles (
+       id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+       name VARCHAR(255) NOT NULL,
+       PRIMARY key(id)
+   );
 
-    -- Create organization table
-    CREATE TABLE Organization (
-        orgId INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
-        orgName VARCHAR(255) NOT NULL,
-        PRIMARY KEY (orgId)
-    );
+   -- Insert roles into roles table
+   INSERT INTO roles (name) VALUES
+   ('E-BOARD'),
+   ('TEAM_LEAD'),
+   ('DEVELOPER'),
+   ('TECH_TEAM'),
+   ('PROJECT_MANAGER');
 
-    -- Create Team table
-    CREATE TABLE Team (
-        teamId INTEGER GENERATED ALWAYS AS IDENTITY,
-        orgId INTEGER NOT NULL,
-        teamName VARCHAR(255),
-        teamLeadId INTEGER,
-        projectManagerId INTEGER,
-        dateCreated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        PRIMARY KEY (teamId),
-        FOREIGN KEY (orgId) REFERENCES Organization(orgId)
-    );
+   -- Create members table
+   CREATE TABLE members (
+       id BIGINT GENERATED ALWAYS AS IDENTITY,
+       team_id BIGINT,
+       name VARCHAR(255) NOT NULL,
+       username VARCHAR(255) NOT NULL,
+       email VARCHAR(255) UNIQUE,
+       password VARCHAR(255) NOT NULL,
+       is_active BOOLEAN,
+       date_joined TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+       PRIMARY KEY (id)
+   );
 
-    -- Create Member table
-    CREATE TABLE Member (
-        memberId INTEGER GENERATED ALWAYS AS IDENTITY,
-        teamId INTEGER,
-        name VARCHAR(255) NOT NULL,
-        username VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE,
-        password VARCHAR(255) NOT NULL,
-        role member_role,
-        isActive BOOLEAN,
-        dateJoined TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (memberId)
-    );
+   -- Create organizations table
+   CREATE TABLE organizations (
+       id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+       name VARCHAR(255) NOT NULL,
+       team_lead_id BIGINT,
+       project_manager_id BIGINT,
+       PRIMARY KEY (id),
+       FOREIGN KEY (team_lead_id) REFERENCES members(id),
+       FOREIGN KEY (project_manager_id) REFERENCES members(id)
+   );
 
-    ALTER TABLE Member
-    ADD CONSTRAINT fk_teamId FOREIGN KEY (teamId) REFERENCES Team(teamId);
+   -- Create teams table
+   CREATE TABLE teams (
+       id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+       organization_id BIGINT NOT NULL,
+       name VARCHAR(255),
+       team_lead_id BIGINT,
+       project_manager_id BIGINT,
+       designer_id BIGINT,
+       date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (organization_id) REFERENCES organizations(id),
+       FOREIGN KEY (team_lead_id) REFERENCES members(id),
+       FOREIGN KEY (project_manager_id) REFERENCES members(id),
+       FOREIGN KEY (designer_id) REFERENCES members(id)
+   );
 
-    ALTER TABLE Team
-    ADD CONSTRAINT fk_teamLeadId FOREIGN KEY (teamLeadId) REFERENCES Member(memberId),
-    ADD CONSTRAINT fk_projectManagerId FOREIGN KEY (projectManagerId) REFERENCES Member(memberId);
+   -- Create member_roles table (to handle many-to-many relationship between members and roles)
+   CREATE TABLE member_roles (
+       member_id BIGINT NOT NULL,
+       role_id BIGINT NOT NULL,
+       PRIMARY KEY (member_id, role_id),
+       FOREIGN KEY (member_id) REFERENCES members(id),
+       FOREIGN KEY (role_id) REFERENCES roles(id)
+   );
 
-    CREATE INDEX idx_member_teamid ON Member(teamId);
-    CREATE INDEX idx_team_orgid ON Team(orgId);
+   --Create blogs table
+   CREATE TABLE blogs (
+       id BIGINT GENERATED ALWAYS AS IDENTITY,
+       author VARCHAR(255) NOT NULL,
+       title VARCHAR(255) NOT NULL,
+       date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+       PRIMARY KEY (id)
+   );
 
-    -- Insert sample organizations
-    INSERT INTO Organization (orgName) VALUES
-    ('Strawberry Fields'),
-    ('Fields of Gold');
+   ALTER TABLE members
+   ADD CONSTRAINT fk_team_id FOREIGN KEY (team_id) REFERENCES teams(id);
 
-    -- Insert sample teams
-    INSERT INTO Team (orgId, teamName, teamLeadId, projectManagerId, dateCreated) VALUES
-    (1, 'TeachTeam', NULL, NULL, '2021-09-01 00:00:00'),
-    (2, 'Backend Team', NULL, NULL, '2024-10-01 00:00:00');
+   -- Insert sample organizations
+   INSERT INTO organizations (name, team_lead_id, project_manager_id) VALUES
+   ('Strawberry Fields', NULL, NULL),
+   ('Fields of Gold',  NULL, NULL);
+
+   -- Insert sample teams
+   INSERT INTO teams (organization_id, name, team_lead_id, project_manager_id, designer_id, date_created) VALUES
+   (1, 'Frontend Team', NULL, NULL, NULL, '2021-09-01 00:00:00'),
+   (2, 'Backend Team', NULL, NULL, NULL, '2024-10-01 00:00:00');
 
     -- Insert sample members
-    INSERT INTO Member (teamId, name, username, email, password, role, isActive) VALUES
-    (1, 'John Doe', 'jdoe', 'jdoe@stevens.edu', 'password', 'E-BOARD', TRUE),
-    (2, 'Jane Smith', 'janesmith', 'janesmith@stevens.edu', 'password2', 'TEAM_LEAD', TRUE),
-    (2, 'Alice Johnson', 'alicej', 'alicej@stevens.edu', 'a1234', 'DEVELOPER', FALSE);
+    INSERT INTO members (team_id, name, username, email, password, is_active) VALUES
+    (1, 'John Doe', 'jdoe', 'jdoe@stevens.edu', 'password', TRUE),
+    (2, 'Jane Smith', 'janesmith', 'janesmith@stevens.edu', 'password2', TRUE),
+    (2, 'Alice Johnson', 'alicej', 'alicej@stevens.edu', 'a1234', FALSE);
 
-    -- Update Team table to set teamLeadId and projectManagerId after members have been inserted
-    UPDATE Team SET teamLeadId = 2, projectManagerId = 2 WHERE teamId = 1;
-    UPDATE Team SET teamLeadId = 1, projectManagerId = 1 WHERE teamId = 2;
+    -- Insert sample member roles
+    INSERT INTO member_roles (member_id, role_id) VALUES
+    (1, 1), -- John Doe as E-BOARD member.
+    (1, 5), -- John Doe as PROJECT_MANAGER.
+    (2, 2), -- Jane Smith as TEAM_LEAD.
+    (3, 3); -- Alice Johnson as DEVELOPER.
+
+    -- Insert sample blogs
+    INSERT INTO blogs (author, title, date_created) VALUES
+    ('John Doe', 'Welcome to the Team!', '2021-09-01 00:00:00'),
+    ('Jane Smith', 'New Project Launch', '2024-10-01 00:00:00');
+
+    -- Update Team table to set team_lead_id, project_manager_id and designer_id after members have been inserted
+    UPDATE teams SET team_lead_id = 2, project_manager_id = 2, designer_id = 3 WHERE id = 1;
+    UPDATE teams SET team_lead_id = 1, project_manager_id = 1, designer_id = 3 WHERE id = 2;
+    UPDATE organizations SET team_lead_id = 2, project_manager_id = 2 WHERE id = 1;
