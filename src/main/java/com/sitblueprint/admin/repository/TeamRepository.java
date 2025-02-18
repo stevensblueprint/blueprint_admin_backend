ca2ac1a2c3c49ecdb6388785510e857caa3a835a
Sofia Martinez <sofiacaro1284@gmail.com>
2025-02-18 11:55:51 -0500

    created custom query for filtering teams by date

--- src/main/java/com/sitblueprint/admin/repository/TeamRepository.java
+++ src/main/java/com/sitblueprint/admin/repository/TeamRepository.java
@@ -2,6 +2,8 @@ package com.sitblueprint.admin.repository;
 
 import com.sitblueprint.admin.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -9,5 +11,6 @@ import  java.util.List;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
-    List<Team> findByDateCreatedBetween(LocalDate start, LocalDate end);
+    @Query("SELECT team FROM Team team WHERE team.dateCreated BETWEEN :start AND :end" )
+    List<Team> findByDateCreatedBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
