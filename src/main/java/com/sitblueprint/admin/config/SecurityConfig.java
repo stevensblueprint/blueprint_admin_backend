eb4632cba0ace069f309404021e704369aa8a26d
miguel-merlin <mmerlin@stevens.edu>
2025-03-25 11:31:33 -0400

    feat: modified cors security config

--- src/main/java/com/sitblueprint/admin/config/SecurityConfig.java
+++ src/main/java/com/sitblueprint/admin/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.sitblueprint.admin.config;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,24 +14,26 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	private static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:3000");
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource)
-			throws Exception {
-		return http.csrf(AbstractHttpConfigurer::disable)
-				.cors(cors -> cors.configurationSource(corsConfigurationSource))
-				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/**").permitAll()
-						.requestMatchers(HttpMethod.POST, "/**").permitAll())
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers("/actuator/**").permitAll()
+						.requestMatchers("/**").permitAll()
+						.anyRequest().authenticated())
+				.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
+						.configurationSource(corsConfigurationSource()))
+				.csrf(AbstractHttpConfigurer::disable)
 				.build();
 	}
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
+	private CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowedOrigins(ALLOWED_ORIGINS);
-		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(List.of("*"));
+		corsConfiguration.setAllowedMethods(List.of("*"));
+		corsConfiguration.setAllowCredentials(false);
 		corsConfiguration.setAllowedHeaders(List.of("*"));
 		corsConfiguration.setMaxAge(3600L);
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfiguration);
 		return source;
